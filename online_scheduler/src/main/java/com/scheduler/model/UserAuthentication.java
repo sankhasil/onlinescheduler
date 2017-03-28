45fc91b0995a0e78fa178842ad82258283aa1a87
sankhasil <sankhasil@gmail.com>
2017-03-29 00:12:07 +0530

    Updated for equals and hashcode methods
    added equals method

--- online_scheduler/src/main/java/com/scheduler/model/UserAuthentication.java
+++ online_scheduler/src/main/java/com/scheduler/model/UserAuthentication.java
@@ -54,7 +54,49 @@ public class UserAuthentication {
 	public void setUser(User user) {
 		this.user = user;
 	}
-	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + ((user_name == null) ? 0 : user_name.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof UserAuthentication)) {
+			return false;
+		}
+		UserAuthentication other = (UserAuthentication) obj;
+		if (password == null) {
+			if (other.password != null) {
+				return false;
+			}
+		} else if (!password.equals(other.password)) {
+			return false;
+		}
+		if (user_name == null) {
+			if (other.user_name != null) {
+				return false;
+			}
+		} else if (!user_name.equals(other.user_name)) {
+			return false;
+		}
+		return true;
+	}
 	
 
 }
